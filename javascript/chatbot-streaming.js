33155bf8dd53c01e689510165209b5f3c13cd1dd
philippart-s <wildagsx@gmail.com>
2024-08-19 14:59:03 +0200

    feat: ✨ JavaScript demo

--- javascript/chatbot-streaming.js
+++ javascript/chatbot-streaming.js
@@ -6,26 +6,10 @@ import {config} from 'dotenv';
 // access the environment variables from the .env file
 config({path: '../.env'});
 
-const model = new ChatMistralAI({
-  modelName: "Mistral-7B-Instruct-v0.2",
-  model: "Mistral-7B-Instruct-v0.2",
-  apiKey: process.env.OVH_AI_ENDPOINTS_ACCESS_TOKEN,
-  endpoint: "https://mistral-7b-instruct-v02.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat",
-  maxTokens: 512
-});
+// js-02-str-model
 
-const promptTemplate = ChatPromptTemplate.fromMessages([
-  ["system", "You are Nestor, a virtual assistant. Answer to the question."],
-  ["human", "{question}"]
-]);
+// js-03-str-prompt
 
-const chain = promptTemplate.pipe(model);
+// js-04-str-chain
 
-const stream = await chain.stream({ question: "What is OVHcloud?" });
-console.log("Nestor 🤖: ");
-
-for await (const chunk of stream) {
-  // Timeout to simulate a human answering the question
-  await setTimeout(50);
-  process.stdout.write(chunk.content);
-}
+// js-05-str-call
