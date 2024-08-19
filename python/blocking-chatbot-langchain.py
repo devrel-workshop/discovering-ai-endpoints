bf7b13d8a676f25e2c122ee058e150ad53280068
philippart-s <wildagsx@gmail.com>
2024-08-19 14:56:19 +0200

    feat: ✨Python demo

--- python/blocking-chatbot-langchain.py
+++ python/blocking-chatbot-langchain.py
@@ -7,18 +7,10 @@ from langchain_core.prompts import ChatPromptTemplate
 # access the environment variables from the .env file
 load_dotenv(dotenv_path='../.env')
 
-model = ChatMistralAI(model="Mistral-7B-Instruct-v0.2", 
-                        api_key=os.getenv('OVH_AI_ENDPOINTS_ACCESS_TOKEN'),
-                        endpoint='https://mistral-7b-instruct-v02.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1', 
-                        max_tokens=512)
+# 07-langchain-blk-model
 
-prompt = ChatPromptTemplate.from_messages([
-  ("system", "You are Nestor, a virtual assistant. Answer to the question."),
-  ("human", "{question}"),
-])
+# 08-langchain-blk-prompt
 
-chain = prompt | model
+# 09-langchain-blk-chain
 
-response = chain.invoke("What is OVHcloud?")
-
-print(f"🤖: {response.content}")
+# 10-langchain-blk-callontent}")
--- python/blocking-chatbot-without-fwk.py
+++ python/blocking-chatbot-without-fwk.py
@@ -5,38 +5,10 @@ import os
 # access the environment variables from the .env file
 load_dotenv(dotenv_path='../.env')
 
-url = "https://mistral-7b-instruct-v02.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1/chat/completions"
-payload = {
-    "max_tokens": 512,
-    "messages": [
-        {
-            "content": "You are Nestor, a virtual assistant. Answer to the question.",
-            "name": "Nestor",
-            "role": "system"
-        },
-        {
-            "content": "What is OvHcloud?",
-            "role": "user"
-        }
-    ],
-    "model": "Mistral-7B-Instruct-v0.2",
-    "temperature": 0,
-}
- 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {os.getenv('OVH_AI_ENDPOINTS_ACCESS_TOKEN')}",
-}
- 
-response = requests.post(url, json=payload, headers=headers)
-if response.status_code == 200:
-    # Handle response
-    response_data = response.json()
-    # Parse JSON response
-    choices = response_data["choices"]
-    for choice in choices:
-        text = choice["message"]["content"]
-        # Process text and finish_reason
-        print(f"🤖: {response.content}")
-else:
-    print("Error:", response.status_code)
+# 02-simple-request
+# 03-simple-sys-msg
+# 04-simple-usr-msg
+
+# 05-simple-token
+
+# 06-simple-call
